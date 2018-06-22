43e30af7c280a95e7fa48996ce77434b87e890bd
Anand Avati <avati@cs.stanford.edu>
2018-06-21 17:09:35 -0700

    fix loss

--- scoring_rules.py
+++ scoring_rules.py
@@ -1,6 +1,6 @@
 
 def MLE_surv(Forecast, Y, C):
-    return - C * Forecast.log_prob(Y) + (1 - Forecast.cdf(Y)).log() * (1 - C)
+    return - (1 - C) * Forecast.log_prob(Y) + (1 - Forecast.cdf(Y)).log() * C
 
 def CRPS_surv(Forecast, Y, C):
     return 0

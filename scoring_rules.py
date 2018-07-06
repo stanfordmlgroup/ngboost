f7560c96c749c7b5cf943c13a4bf015968b0a0cf
Anand Avati <avati@cs.stanford.edu>
2018-07-06 14:44:10 -0700

    natural gradients with MLE

--- scoring_rules.py
+++ scoring_rules.py
@@ -1,26 +1,78 @@
 import torch
 import numpy as np
 
-def MLE_surv(Forecast, Y, C):
-    return - ((1 - C) * Forecast.log_prob(Y) + (1 - Forecast.cdf(Y) + 1e-5).log() * C)
+class MLE_surv(object):
+    def __init__(self, K=32):
+        self.K = K
+        pass
 
-def CRPS_surv(Forecast, Y, C):
-    def I(F, U):
-        I_sum = 0.
-        for th in np.linspace(0, 1., 8):
-            if th == 0:
-                prev_F = 0.
-                prev_x = 0.
-                continue
-            this_x = U * th
-            this_F = F(this_x)
-            Fdiff = 0.5 * (this_F + prev_F)
-            xdiff = this_x - prev_x
-            I_sum += (Fdiff * xdiff)
-            prev_F = this_F
-            prev_x = this_x
-        return I_sum
+    def loss(self, Forecast, Y, C):
+        return - ((1 - C) * Forecast.log_prob(Y) + (1 - Forecast.cdf(Y) + 1e-5).log() * C)
 
-    left = I(lambda y: Forecast.cdf(y).pow(2), Y)
-    right = I(lambda y: ((1 - Forecast.cdf(1/y)) / y).pow(2), 1/Y)
-    return (left + (1 - C) * right)
+    def __call__(self, Forecast, Y, C):
+        return self.loss(Forecast, Y, C)
+
+    def inverse(self, matrix):
+        m = int(matrix.shape[0])
+        return torch.inverse(matrix + 1e-2 * torch.eye(m))
+
+    def grad(self, params, D):
+        grad = [p.grad.clone() for p in params]
+        grads = torch.cat([g.reshape(-1, 1) for g in grad], dim=1)
+        Forecast = D(params)
+        metric = self.metric(params, Forecast)
+        nat_grads = torch.cat([torch.mv(self.inverse(m), g).unsqueeze(0) for g, m in zip(grads, metric)], dim=0)
+        nat_grad = [ng.reshape(-1) for ng in torch.split(nat_grads, 1, dim=1)]
+        return nat_grad
+
+    def metric(self, params, Forecast):
+        cov = 0
+        m, n = int(params[0].shape[0]), len(params)
+        for _ in range(self.K):
+            for p in params:
+                p.grad.data.zero_()
+            X = Forecast.sample()
+            score = Forecast.log_prob(X).mean()
+            score.backward(retain_graph=True)
+            grads = torch.cat([p.grad.clone().reshape(-1, 1) for p in params], dim=1)
+            cov += grads.reshape(m, 1, n) * grads.reshape(m, n, 1)
+        return cov / self.K
+
+class CRPS_surv(object):
+    def __init__(self, K=32):
+        self.K = K
+        pass
+
+    def loss(self, Forecast, Y, C):
+        def I(F, U):
+            I_sum = 0.
+            for th in np.linspace(0, 1., self.K):
+                if th == 0:
+                    prev_F = 0.
+                    prev_x = 0.
+                    continue
+                this_x = U * th
+                this_F = F(this_x)
+                Fdiff = 0.5 * (this_F + prev_F)
+                xdiff = this_x - prev_x
+                I_sum += (Fdiff * xdiff)
+                prev_F = this_F
+                prev_x = this_x
+            return I_sum
+
+        left = I(lambda y: Forecast.cdf(y).pow(2), Y)
+        right = I(lambda y: ((1 - Forecast.cdf(1/y)) / y).pow(2), 1/Y)
+        return (left + (1 - C) * right)
+
+    def __call__(self, Forecast, Y, C):
+        return self.loss(Forecast, Y, C)
+
+    def grad(self, params, D):
+        grads = [torch.Tensor(p.grad) for p in params]
+        Forecast = D(params)
+        metrics = self.metric(params, Forecast)
+        
+        return grads
+
+    def metric(self, params, Forecast):
+        return 0
--- survboost.py
+++ survboost.py
@@ -19,9 +19,10 @@ class SurvBoost(object):
         self.minibatch_frac = minibatch_frac
         self.Dist = Dist
         self.D = lambda args: Dist(*[transform_to(constraint)(arg) for (param, constraint), arg in zip(Dist.arg_constraints.items(), args)])
-        self.Score = Score
+        self.Score = Score()
         self.Base = Base
         self.base_models = []
+        self.scalings = []
 
     def init_params(self, size):
         Z = lambda l: np.zeros(size).astype(np.float32)
@@ -31,23 +32,25 @@ class SurvBoost(object):
         m, n = X.shape
         params = self.init_params(m)
 
-        for models in self.base_models:
+        for models, scalings in zip(self.base_models, self.scalings):
             base_params = [model.predict(X) for model in models]
-            params = [p - self.learning_rate * b for p, b in zip(params, base_params)]
+            params = [p - self.learning_rate * b for p, b in zip(params, self.mul(base_params, scalings))]
         
         return [torch.tensor(p, requires_grad=True, dtype=torch.float32) for p in params]
 
     def sample(self, X, Y, C):
         sample_size = int(self.minibatch_frac * len(Y))
         idxs = np.random.choice(np.arange(len(Y)), sample_size, replace=False)
-        return X[idxs,:], torch.Tensor(Y[idxs]), torch.Tensor(C[idxs])
+        return idxs, X[idxs,:], torch.Tensor(Y[idxs]), torch.Tensor(C[idxs])
 
-    def fit_base(self, X, resids):
-        models = [self.Base().fit(X, rs) for rs in resids]
+    def fit_base(self, X, grads):
+        models = [self.Base().fit(X, g) for g in grads]
         self.base_models.append(models)
+        fitted = [torch.Tensor(m.predict(X)) for m in models]
+        return fitted
 
     def mul(self, array, num):
-        return [a * num for a in array]
+        return [a * n for (a, n) in zip(array, num)]
 
     def sub(self, A, B):
         return [a - b for a, b in zip(A, B)]
@@ -56,44 +59,49 @@ class SurvBoost(object):
         # of course this is not the norm, but serves the purpose
         return sum([float(torch.norm(g)) for g in grads])
 
-    def line_search(self, fn, start, grad):
-        loss_init = fn(start)
-        scale = 1.
-        while float(fn(self.sub(start, self.mul(grad, scale)))) > float(loss_init):
-            scale = scale / 2.
+    def line_search(self, fn, start, resids):
+        loss_init = float(fn(start))
+        scale = [1. for _ in resids]
+        half = [0.5 for _ in resids]
+        while True:
+            new_loss = float(fn(self.sub(start, self.mul(resids, scale))))
+            if new_loss < loss_init:
+                break
+            scale = self.mul(scale, half)
+        self.scalings.append(scale)
         return scale
 
     def fit(self, X, Y, C):
         for itr in range(self.n_estimators):
-            X_batch, Y_batch, C_batch = self.sample(X, Y, C)
+            idxs, X_batch, Y_batch, C_batch = self.sample(X, Y, C)
+
+            S = lambda p: self.Score(self.D(p), Y_batch, C_batch).mean()
 
             params = self.pred_param(X_batch)
 
-            Forecast = self.D(params)
+            score = S(params)
 
-            score = self.Score(Forecast, Y_batch, C_batch).mean()
             print('[iter %d] loss=%f' % (itr, float(score)))
             if float(score) == float('-inf'):
                 break
             if str(float(score)) == 'nan':
                 print(params)
-                print(self.Score(Forecast, Y_batch, C_batch))
+                print(S(params))
                 for (m, s, y, c) in zip(params[0], params[1], Y_batch, C_batch):
                     ln = LogNormal(m, s.exp())
                     print(float(m), float(s), float(y), float(c), float(ln.log_prob(y)), float(ln.cdf(y)))
                 break
 
-            score.backward()
+            score.backward(retain_graph=True)
 
-            grads = [p.grad for p in params]
+            grads = self.Score.grad(params, self.D)
 
-            scale = self.line_search(lambda p_: self.Score(self.D(p_), Y_batch, C_batch).mean(), params, grads)
+            resids = self.fit_base(X_batch, grads)
 
-            resids = self.mul(grads, scale)
-            if self.norm(resids) < 1e-5:
+            scale = self.line_search(S, params, resids)
+
+            if self.norm(self.mul(resids, scale)) < 1e-3:
                 break
-
-            self.fit_base(X_batch, self.mul(resids, scale))
 
     def pred_dist(self, X):
         params = self.pred_param(X)
@@ -116,8 +124,8 @@ def main():
 
     sb = SurvBoost(Base = lambda : DecisionTreeRegressor(criterion='mse'),
                    Dist = LogNormal,
-                   Score = CRPS_surv,
-                   n_estimators = 200)
+                   Score = MLE_surv,
+                   n_estimators = 1000)
     sb.fit(X, Y, C)
     preds_dt = sb.pred_mean(X)
     # print(sb.pred_mean(X))
@@ -128,6 +136,7 @@ def main():
 #     sb.fit(X, Y, C)
 #     preds_lin = sb.pred_mean(X)
 
+    print(sb.pred_dist(X[C == 1]).cdf(torch.tensor(Y[C == 1])))
     print("Train/DecTree:", calculate_concordance_naive(preds_dt, Y, C))
     print('Pred_mean: %f, True_mean: %f' % (np.mean(preds_dt), np.mean(Y)))
 #    print("Train/LinReg:", calculate_concordance_naive(preds_lin, Y ,C))

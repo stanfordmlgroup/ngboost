ae7f1f69a429d181fad6f53bb09c7d017b8e83f3
Tony Duan <tony@tonyduan.com>
2018-07-22 19:01:39 -0700

    Remove randomness from sanity check.

--- experiments/sanity_check.py
+++ experiments/sanity_check.py
@@ -11,20 +11,24 @@ from ngboost.scores import MLE_surv, CRPS_surv
 def main():
     m, n = 100, 50
     X = np.random.rand(m, n).astype(np.float32)
-    Y = np.random.rand(m).astype(np.float32) * 2 + 1
+    # Y = np.random.rand(m).astype(np.float32) * 2 + 1
+    Y = np.sum(X, axis=1)
+    Y = (Y - np.mean(Y)) / np.std(Y)
+    Y = Y - np.min(Y) + 1e-2
     C = (np.random.rand(m) > 0.5).astype(np.float32)
+    # C = np.zeros_like(Y)
 
     sb = SurvNGBoost(Base = lambda : DecisionTreeRegressor(criterion='mse'),
                      Dist = LogNormal,
-                     Score = CRPS_surv,
+                     Score = MLE_surv,
                      n_estimators = 12,
                      learning_rate = 0.1,
                      natural_gradient = True,
                      second_order = True,
                      quadrant_search = False,
                      nu_penalty=1e-5)
-    
-    sb.fit(X[:70], Y[:70], C[:70], X[70:], Y[70:], C[70:])
+
+    train, test = sb.fit(X[:70], Y[:70], C[:70], X[70:], Y[70:], C[70:])
     preds = sb.pred_mean(X)
 
     print("Train/DecTree:", calculate_concordance_naive(preds, Y, C))

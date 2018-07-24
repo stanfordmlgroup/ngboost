be3e8daefabcebcaf924abd0c7a6a2f5c34d13e1
Daisy Yi Ding <dingd@stanford.edu>
2018-07-23 17:52:48 -0700

    empirical experiments on regression

--- experiments/empirical.py
+++ experiments/empirical.py
@@ -4,25 +4,29 @@ import numpy as np
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from distns import HomoskedasticNormal
 from torch.distributions import Normal
 
-from ngboost import NGBoost, MLE, CRPS
+from distns import HomoskedasticNormal
+from ngboost.ngboost import NGBoost, SurvNGBoost
 from experiments.evaluation import r2_score
+from ngboost.scores import MLE_surv, CRPS_surv
 
 if __name__ == "__main__":
 
     data = load_boston()
     X, y = data["data"], data["target"]
+    C = np.zeros(len(y))
 
 #    y = ( y - np.mean(y) ) / np.std(y)
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    X_train, X_test, y_train, y_test, C_train, C_test = train_test_split(X, y, C)
 
-    ngb = NGBoost(Base=lambda: DecisionTreeRegressor(criterion='mse'),
-                  Dist=Normal,
-                  Score=CRPS,
-                  n_estimators=100,
-                  learning_rate=0.5,
+    ngb = SurvNGBoost(Base=lambda: DecisionTreeRegressor(criterion='mse'),
+                  Dist=HomoskedasticNormal,
+                  Score=MLE_surv,
+                  n_estimators=200,
+                  learning_rate=0.02,
                   natural_gradient=True,
                   second_order=True,
                   quadrant_search=False,
@@ -30,10 +34,10 @@ if __name__ == "__main__":
                   nu_penalty=1e-5,
                   verbose=True)
 
-    ngb.fit(X_train, y_train)
+    ngb.fit(X_train, y_train, C_train, X_test, y_test, C_test)
     y_pred = ngb.pred_mean(X_test)
     print(r2_score(y_test, y_pred))
 
-    # gbr = GradientBoostingRegressor()
-    # gbr.fit(X_train, y_train)
-    # print(r2_score(y_test, gbr.predict(X_test)))
+    gbr = GradientBoostingRegressor()
+    gbr.fit(X_train, y_train)
+    print(r2_score(y_test, gbr.predict(X_test)))

9e950d0d34eefffdcd63f9fa6fec2c877dbd622c
Tony Duan <tony@tonyduan.com>
2018-09-10 01:13:36 -0400

    Add verbose.

--- scripts/run_all_regression_crps.sh
+++ scripts/run_all_regression_crps.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-python3 -m experiments.regression --dataset housing --score=CRPS
-python3 -m experiments.regression --dataset concrete --score=CRPS
-python3 -m experiments.regression --dataset wine --score=CRPS
-python3 -m experiments.regression --dataset kin8nm --score=CRPS
-python3 -m experiments.regression --dataset naval --score=CRPS
-python3 -m experiments.regression --dataset power --score=CRPS
-python3 -m experiments.regression --dataset energy --score=CRPS
-python3 -m experiments.regression --dataset protein --score=CRPS
+python3 -m experiments.regression --dataset housing --score=CRPS --verbose
+python3 -m experiments.regression --dataset concrete --score=CRPS --verbose
+python3 -m experiments.regression --dataset wine --score=CRPS --verbose
+python3 -m experiments.regression --dataset kin8nm --score=CRPS --verbose
+python3 -m experiments.regression --dataset naval --score=CRPS --verbose
+python3 -m experiments.regression --dataset power --score=CRPS --verbose
+python3 -m experiments.regression --dataset energy --score=CRPS --verbose
+python3 -m experiments.regression --dataset protein --score=CRPS --verbose
--- scripts/run_all_regression_mle.sh
+++ scripts/run_all_regression_mle.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-python3 -m experiments.regression --dataset housing --score=MLE
-python3 -m experiments.regression --dataset concrete --score=MLE
-python3 -m experiments.regression --dataset wine --score=MLE
-python3 -m experiments.regression --dataset kin8nm --score=MLE
-python3 -m experiments.regression --dataset naval --score=MLE
-python3 -m experiments.regression --dataset power --score=MLE
-python3 -m experiments.regression --dataset energy --score=MLE
-python3 -m experiments.regression --dataset protein --score=MLE
+python3 -m experiments.regression --dataset housing --score=MLE --verbose
+python3 -m experiments.regression --dataset concrete --score=MLE --verbose
+python3 -m experiments.regression --dataset wine --score=MLE --verbose
+python3 -m experiments.regression --dataset kin8nm --score=MLE --verbose
+python3 -m experiments.regression --dataset naval --score=MLE --verbose
+python3 -m experiments.regression --dataset power --score=MLE --verbose
+python3 -m experiments.regression --dataset energy --score=MLE --verbose
+python3 -m experiments.regression --dataset protein --score=MLE --verbose

b37d509b4390610ab0760bb48690bb1df22403fa
Tony Duan <tony@tonyduan.com>
2018-09-11 17:55:31 -0400

    Refactor learners.

--- ngboost/learners.py
+++ ngboost/learners.py
@@ -2,9 +2,9 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
 
 
-default_tree_learner = lambda: DecisionTreeRegressor(
+tree_learner_with_depth = lambda d: DecisionTreeRegressor(
     criterion='friedman_mse', min_samples_split=2, min_samples_leaf=1,
-    min_weight_fraction_leaf=0.0, max_depth=3)
+    min_weight_fraction_leaf=0.0, max_depth=d)
 
-
+default_tree_learner = lambda: tree_learner_with_depth(3)
 default_linear_learner = lambda: LinearRegression()

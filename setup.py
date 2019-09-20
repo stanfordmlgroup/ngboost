117f272958084abba3cda6e7dce95fbd0b2f2e3e
Tony Duan <tony@tonyduan.com>
2019-09-20 10:28:06 -0700

    Add setup file.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="ngboost",
+    version="0.0.1",
+    author="Stanford ML Group",
+    author_email="{avati,tonyduan}@cs.stanford.edu",
+    description="NGBoost.",
+    long_description="Please see Github for full description.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/avati/ngboost",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

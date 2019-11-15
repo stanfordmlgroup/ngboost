bcfdc109298d1e5597605b72ae79d1c130a31f52
Tony Duan <tony@tonyduan.com>
2019-11-14 23:01:57 -0800

    Add Exponential distribution CRPS.

--- ngboost/distns/exponential.py
+++ ngboost/distns/exponential.py
@@ -8,54 +8,48 @@ class Exponential(object):
     n_params = 1
 
     def __init__(self, params):
-        self.loc = 0
         self.scale = np.exp(params[0])
-        self.shp = self.scale.shape
-
         self.dist = dist(scale=self.scale)
 
     def __getattr__(self, name):
         return getattr(self.dist, name)
 
     def nll(self, Y):
-        try:
-            E = Y['Event']
-            T = Y['Time']
-            cens = (1-E) * np.log(1 - self.dist.cdf(T) + eps)
-            uncens = E * self.dist.logpdf(T)
-            return -(cens + uncens)
-        except:
-            return -self.dist.logpdf(Y)
+        E, T = Y["Event"], Y["Time"]
+        cens = (1-E) * np.log(1 - self.dist.cdf(T) + eps)
+        uncens = E * self.dist.logpdf(T)
+        return -(cens + uncens)
 
     def D_nll(self, Y):
-        try:
-            E = Y['Event']
-            T = Y['Time']
-            cens = (1-E) * T.squeeze() / self.scale
-            uncens = E * (-1 + T.squeeze() / self.scale)
-            return -(cens + uncens).reshape((-1, 1))
-        except:
-            return (-1 + Y.squeeze() / self.scale).reshape((-1, 1))
+        E, T = Y["Event"], Y["Time"]
+        cens = (1-E) * T.squeeze() / self.scale
+        uncens = E * (-1 + T.squeeze() / self.scale)
+        return -(cens + uncens).reshape((-1, 1))
 
     def fisher_info(self):
-        FI = np.ones((self.scale.shape[0], 1, 1))
+        FI = 2 * np.ones((self.scale.shape[0], 1, 1))
         return FI
 
     def crps(self, Y):
-        try:
-            E = Y['Event']
-            T = Y['Time']
-            return None
-        except:
-            return None
-        pass
+        E = Y['Event']
+        T = Y['Time']
+        c = E == 1
+        score = T + self.scale * (2 * np.exp(-T / self.scale) - 1.5)
+        score[c] -= 0.5 * self.scale[c] * np.exp(-2 * T[c] / self.scale[c])
+        return score
 
     def D_crps(self, Y):
-        pass
+        E = Y["Event"]
+        T = Y["Time"]
+        c = E == 1
+        d = 2 * np.exp(-T/self.scale) * (self.scale + T) - 1.5 * self.scale
+        d[c] -= np.exp(-2 * T[c] / self.scale[c]) * (0.5 * self.scale[c] - T[c])
+        return deriv.reshape((-1, 1))
 
     def crps_metric(self):
-        pass
+        M = 0.5 * self.scale[:, np.newaxis, np.newaxis]
+        return M
 
     def fit(Y):
-        m, s = sp.stats.expon.fit(Y)
+        m, s = sp.stats.expon.fit(Y["Time"])
         return np.array([np.log(m + s)])

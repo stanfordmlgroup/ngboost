a217a9efd6f3ed108b8a6bff2f3d0e2fc5b60783
yutayamazaki <tppymd@gmail.com>
2019-11-16 12:04:05 +0900

    add predict_proba method to NGBClassifier

--- ngboost/sklearn_api.py
+++ ngboost/sklearn_api.py
@@ -18,9 +18,15 @@ class NGBClassifier(NGBoost, ClassifierMixin):
         Dist need to be Bernoulli.
         You can use this model for only binary classification.
     """
-    def __init__(self, *args, **kwargs):
-        super(NGBClassifier, self).__init__(Dist=Bernoulli, *args, **kwargs)
 
     def predict(self, X):
         dist = self.pred_dist(X)
         return np.round(dist.prob)
+
+    def predict_proba(self, X):
+        num_classes = 2
+        y_pred = np.zeros((len(X), num_classes))
+        dist = self.pred_dist(X)
+        y_pred[:, 1] = dist.prob
+        y_pred[:, 0] = 1 - dist.prob
+        return y_pred

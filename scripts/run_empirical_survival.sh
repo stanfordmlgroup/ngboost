8790198fb0985224e53f6bb89d7cab00afd6cf63
Tony Duan <tony@tonyduan.com>
2019-11-20 09:48:48 -0800

    Rename files in scripts.

--- scripts/run_empirical_survival.sh
+++ scripts/run_empirical_survival.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-python3 -m examples.empirical.survival --dataset=flchain  --score=MLE --natural --verbose --n-est=1000 --lr=0.1  --distn=LogNormal
+python3 -m examples.experiments.survival_exp --dataset=flchain  --score=MLE --natural --verbose --n-est=1000 --lr=0.1  --distn=LogNormal
 
 exit
 
-python3 -m examples.empirical.survival --dataset=support  --score=MLE --natural --verbose
-python3 -m examples.empirical.survival --dataset=sprint  --score=MLE --natural --verbose
+python3 -m examples.experiments.survival_exp --dataset=support  --score=MLE --natural --verbose
+python3 -m examples.experiments.survival_exp --dataset=sprint  --score=MLE --natural --verbose
 
 exit
 
-python3 -m examples.empirical.survival --dataset=flchain  --score=CRPS --natural --verbose --distn=LogNormal
-python3 -m examples.empirical.survival --dataset=support  --score=CRPS --natural --verbose
-python3 -m examples.empirical.survival --dataset=sprint  --score=CRPS --natural --verbose
+python3 -m examples.experiments.survival_exp --dataset=flchain  --score=CRPS --natural --verbose --distn=LogNormal
+python3 -m examples.experiments.survival_exp --dataset=support  --score=CRPS --natural --verbose
+python3 -m examples.experiments.survival_exp --dataset=sprint  --score=CRPS --natural --verbose

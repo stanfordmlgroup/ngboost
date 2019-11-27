b50622a2844f0c452adc09a2e6395fc706484c31
Michael <mlko53@stanford.edu>
2019-11-27 15:14:52 -0800

    update survival scripts

--- ngboost/distns/multivariate_normal.py
+++ ngboost/distns/multivariate_normal.py
@@ -22,10 +22,8 @@ class MultivariateNormal(object):
         self.cov_inv = np.linalg.inv(self.cov)
         self.dCovdL = self.D_cov_D_L()
 
-    def __getattr__(self, name):
-        if name in dir(self.dist):
-            return getattr(self.dist, name)
-        return None
+    def mean(self):
+        return np.exp(self.loc[:,0])
 
     def D_cov_D_L(self):
         # create commutation matrix

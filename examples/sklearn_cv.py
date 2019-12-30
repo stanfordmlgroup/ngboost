0bc4b50cb2321fdcee164e807e91f7d419772b08
Alejandro Schuler <alejandro.x.schulerdacostaferro@kp.org>
2019-12-29 20:27:02 -0800

    An example where the base learner is also searched over using CV

--- examples/sklearn_cv.py
+++ examples/sklearn_cv.py
@@ -23,3 +23,31 @@ if __name__ == "__main__":
     grid_search = GridSearchCV(ngb, param_grid=param_grid, cv=5)
     grid_search.fit(X_train, y_train)
     print(grid_search.best_params_)
+
+
+# An example where the base learner is also searched over (this is how you would vary tree depth):
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import Ridge
+
+X, Y = load_breast_cancer(True)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+b1 = DecisionTreeRegressor(criterion='friedman_mse', max_depth=2)
+b2 = DecisionTreeRegressor(criterion='friedman_mse', max_depth=4)
+b3 = Ridge(alpha=0.0)
+
+param_grid = {
+    'n_estimators': [20, 50],
+    'minibatch_frac': [1.0, 0.5],
+    'Base': [b1, b2]
+}
+
+ngb = NGBRegressor(
+    natural_gradient=True,
+    verbose=False,
+    Dist=Normal
+)
+
+grid_search = GridSearchCV(ngb, param_grid=param_grid, cv=5)
+grid_search.fit(X_train, y_train)
+print(grid_search.best_params_)

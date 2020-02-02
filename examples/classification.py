de50ee4a91c79cd2b8d7b585a70c1930481aa0a9
Tony Duan <tony@tonyduan.com>
2020-02-01 22:32:10 -0800

    Minor update to classification example.

--- examples/classification.py
+++ examples/classification.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ngboost import NGBClassifier
 from ngboost.distns import Bernoulli
 from ngboost.learners import default_tree_learner
@@ -10,10 +11,13 @@ from sklearn.metrics import roc_auc_score
 
 if __name__ == "__main__":
 
+    np.random.seed(12345)
+
     X, Y = load_breast_cancer(True)
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
-    ngb = NGBClassifier(Base=default_tree_learner, Dist=Bernoulli, Score=MLE, verbose=True)
+    ngb = NGBClassifier(Base=default_tree_learner, Dist=Bernoulli, Score=MLE, verbose=True,
+                        natural_gradient=True)
     ngb.fit(X_train, Y_train)
 
     preds = ngb.pred_dist(X_test)

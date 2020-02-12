3acfddea0911e30e671d94c5d31517d0e515efd9
Alejandro Schuler <alejandro.schuler@gmail.com>
2020-02-11 23:32:06 -0800

    Update helpers.py

--- ngboost/helpers.py
+++ ngboost/helpers.py
@@ -5,7 +5,7 @@ def Y_from_censored(T,E=None):
         return None
     if E is None:
     	E = np.ones_like(T)
-    Y = np.empty(dtype=[('Event', np.bool), ('Time', np.float64)],
+    Y = np.empty(dtype=[('Event', bool), ('Time', np.float64)],
                  shape=T.shape[0])
     Y['Event'] = E.astype(bool)
     Y['Time'] = T.astype(float)

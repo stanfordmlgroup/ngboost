3f7797e202325129f79ff338f39e6e9d0684d049
guyko81 <guyko81@users.noreply.github.com>
2020-06-05 12:48:07 +0100

    new grad

--- ngboost/ngboost.py
+++ ngboost/ngboost.py
@@ -228,7 +228,7 @@ class NGBoost(object):
             loss_list += [train_loss_monitor(D, Y_batch, weight_batch)]
             loss = loss_list[-1]
             grads = D.grad(Y_batch, natural=self.natural_gradient)
-            print("grad: ", grads)
+            #print("grad: ", grads)
             proj_grad = self.fit_base(X_batch, grads, weight_batch)
             #print("proj_grad: ", proj_grad)
             scale = self.line_search(proj_grad, P_batch, Y_batch, weight_batch)
@@ -240,7 +240,7 @@ class NGBoost(object):
                 * scale
                 * np.array([m.predict(X[:, col_idx]) for m in self.base_models[-1]]).T
             )
-            #print(params)
+            print(params)
             
             val_loss = 0
             if X_val is not None and Y_val is not None:

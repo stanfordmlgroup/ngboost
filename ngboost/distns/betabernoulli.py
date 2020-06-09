ecb6cab513cada234d5aa51565c278bbabed06a9
guyko81 <guyko81@users.noreply.github.com>
2020-06-09 11:15:02 +0100

    negative LL

--- ngboost/distns/betabernoulli.py
+++ ngboost/distns/betabernoulli.py
@@ -14,20 +14,37 @@ class BetaBernoulliLogScore(LogScore):
 
     def d_score(self, Y):
         D = np.zeros((len(Y), 2)) # first col is dS/d(log(α)), second col is dS/d(log(β))
-        p = betadist(a=self.alpha, b=self.beta).mean()
 
-        D[:, 0] =   self.alpha * (
+        D[:, 0] =   -self.alpha * (
                         digamma(self.alpha + self.beta) + 
                         digamma(Y + self.alpha) -
                         digamma(self.alpha + self.beta + 1) -
                         digamma(self.alpha)
                     )
-        D[:, 1] =   self.beta * (
+        # D[:, 0] =   (
+        #                 digamma(np.exp(self.m_raw)/(1 + np.exp(-self.mu_raw))) -
+        #                 digamma(Y + np.exp(self.m_raw)/(1 + np.exp(-self.mu_raw))) -
+        #                 digamma(np.exp(self.m_raw) - np.exp(self.m_raw)/(1 + np.exp(-self.mu_raw))) +
+        #                 digamma(-Y + np.exp(self.m_raw) + 1 - np.exp(self.m_raw)/(1 + np.exp(-self.mu_raw)))
+        #             ) * np.exp(self.m_raw) * np.exp(-self.mu_raw) / (np.exp(self.mu_raw) + 1)**2
+        D[:, 1] =   -self.beta * (
                         digamma(self.alpha + self.beta) + 
                         digamma(-Y + self.beta + 1) -
                         digamma(self.alpha + self.beta + 1) -
                         digamma(self.beta)
                     )
+        # D[:, 1] =   (
+        #                 np.exp(self.mu_raw) * (
+        #                     digamma(np.exp(self.m_raw) / (1 + np.exp(-self.mu_raw))) -
+        #                     digamma(Y + np.exp(self.m_raw) / (1 + np.exp(-self.mu_raw))) +
+        #                     digamma(np.exp(self.m_raw) + 1) -
+        #                     digamma(np.exp(self.m_raw))
+        #                 ) +
+        #                 digamma(np.exp(self.m_raw) + 1) +
+        #                 digamma(np.exp(self.m_raw) - np.exp(self.m_raw) / (1 + np.exp(-self.mu_raw))) -
+        #                 digamma(-Y + np.exp(self.m_raw) + 1 - np.exp(self.m_raw)/(1 + np.exp(-self.mu_raw))) -
+        #                 digamma(np.exp(self.m_raw))
+        #             ) * np.exp(self.m_raw) / (np.exp(self.mu_raw) + 1)
         return D
 
 class BetaBernoulli(RegressionDistn):
@@ -40,11 +57,21 @@ class BetaBernoulli(RegressionDistn):
         self._params = params
         
         # create other objects that will be useful later
+        # self.mu_raw = params[0]
+        # self.m_raw = params[1]
+        # self.mu = self.sigmoid(self.mu_raw)
+        # self.m = np.exp(self.m_raw)
+        # self.alpha = self.mu * self.m # mu = alpha/(alpha + beta)
+        # self.beta = self.m * (1 - self.mu) # m = alpha + beta
+        # self.dist = dist(n=1, a=self.alpha, b=self.beta)
         self.log_alpha = params[0]
-        self.log_beta = params[1]
-        self.alpha = np.exp(params[0]) # since params[1] is log(alpha)
-        self.beta = np.exp(params[1]) # since params[1] is log(beta)
+        self.log_beta = params[0]
+        self.alpha = np.exp(self.log_alpha)
+        self.beta = np.exp(self.log_beta)
         self.dist = dist(n=1, a=self.alpha, b=self.beta)
+
+    def sigmoid(self, x):
+        return 1/(1+np.exp(-x))
 
     def fit(Y):
         imps = np.ones_like(Y)

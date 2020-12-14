build class methods, fix sign issue

ngboost/distns/distn.py
@@ -74,22 +74,29 @@ class RegressionDistn(Distn):
             np.log, self._pdf
         )  # y, params -> log-likelihood (scalar)
 
-    @classmethod
-    def derive_cdf(cls):
-        return lambda y, params: cls.cdf(y, **cls.params_to_user(params))
-
-    @classmethod
-    def derive_pdf(cls):
-        cdf = cls.derive_cdf()
-        return grad(cdf)
-
-    @classmethod
-    def derive_logpdf(cls):
-        pdf = cls.derive_pdf()
-        return compose(np.log, pdf)
-
     def predict(self):  # predictions for regression are typically conditional means
         return self.mean()
+
+    @classmethod
+    def build(cls):
+        class BuiltDist(cls):
+
+            if not hasattr(cls, "cdf"):
+                raise ValueError(
+                    f"The distribution {cls.__name__} has no CDF defined."
+                    f" clsributions for regression must define at least a CDF."
+                )
+
+            if not hasattr(cls, "_cdf"):
+                _cdf = lambda y, params: cls.cdf(y, **cls.params_to_user(params))
+
+            if not hasattr(cls, "_likelihood"):
+                _likelihood = grad(_cdf)
+
+            if not hasattr(cls, "_nll"):
+                _nll = compose(lambda x: -x, np.log, _likelihood)
+
+        return BuiltDist
 
 
 class ClassificationDistn(Distn):

ngboost/distns/normal.py
@@ -11,6 +11,8 @@ from ngboost.scores import CRPScore, LogScore
 
 
 class NormalLogScore(LogScore):
+    pass
+
     @classmethod
     def metric(cls, _params):
         loc, scale = Normal.params_to_user(_params).values()
@@ -62,10 +64,6 @@ class Normal(RegressionDistn):
         return norm.cdf(Y, loc=loc, scale=scale)
 
     # @classmethod
-    # def pdf(cls, Y, loc, scale):
-    #     return norm.pdf(Y, loc=loc, scale=scale)
-
-    # @classmethod
     # def logpdf(cls, Y, loc, scale):
     #     return norm.logpdf(Y, loc=loc, scale=scale)
 
@@ -76,9 +74,7 @@ class Normal(RegressionDistn):
 
     ### Automatable?
     def sample(self, m):  # automate based on cdf?
-        return np.array(
-            [sp.stats.norm.rvs(**self.params_to_user(self._params)) for i in range(m)]
-        )
+        return np.array([sp.stats.norm.rvs(**self.params) for i in range(m)])
 
     @classmethod
     def fit(cls, Y):  # automate based on cdf?

ngboost/scores.py
@@ -7,12 +7,12 @@ import pdb
 
 class Score:
     @classmethod
-    def total_score(cls, Y, _params, sample_weight=None):
+    def _total_score(cls, Y, _params, sample_weight=None):
         # pdb.set_trace()
         return np.average(cls.score(Y, _params), weights=sample_weight)
 
     @classmethod
-    def grad(cls, Y, _params, natural=True):
+    def _grad(cls, Y, _params, natural=True):
         grad = cls.d_score(Y, _params)
         if natural:
             metric = cls.metric(_params)
@@ -29,24 +29,27 @@ class LogScore(Score):
     method for calculating the Riemannian metric.
     """
 
-    # def __init__(self):
-    #     self.score = jit(lambda y: vmap(self._logpdf)(y, self._params))
-    #     self.d_score = jit(lambda y: vmap(grad(self._logpdf, 1))(y, self._params))
-
-    @classmethod
-    def derive_score(cls, Dist):
-        return jit(vmap(Dist._logpdf))
-
-    @classmethod
-    def derive_d_score(cls, Dist):
-        return jit(vmap(grad(Dist._logpdf, 1)))
-
     @classmethod
     def metric(cls, _params, n_mc_samples=100):
-        grads = np.stack([self.d_score(Y, _params) for Y in self.sample(n_mc_samples)])
+        grads = np.stack(
+            [cls.d_score(Y, _params) for Y in cls(_params).sample(n_mc_samples)]
+        )
         return np.mean(np.einsum("sik,sij->sijk", grads, grads), axis=0)
 
-    # autofit method from d_score?
+    @classmethod
+    def build(cls, Dist):
+        ImplementedScore = Dist.implementation(cls)
+
+        class BuiltScore(ImplementedScore):
+            if not hasattr(ImplementedScore, "score"):
+                score = jit(vmap(Dist._nll))
+
+            if not hasattr(ImplementedScore, "d_score"):
+                d_score = jit(vmap(grad(Dist._nll, 1)))
+
+        return BuiltScore
+
+    # autofit method from d_score? Simple gradient descent?
 
 
 MLE = LogScore
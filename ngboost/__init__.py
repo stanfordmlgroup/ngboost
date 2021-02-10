ac103a42c83512b7b6925ac7d6032d8b22453141
zyxue <alfred532008@gmail.com>
2021-02-10 08:57:26 -0800

    supported version checking via ngboost.__version__

--- ngboost/__init__.py
+++ ngboost/__init__.py
@@ -1,3 +1,13 @@
 """The NGBoost Library"""
+
+try:
+    from importlib.metadata import version
+except ImportError:
+    # before python 3.8
+    from importlib_metadata import version
+
 from .api import NGBClassifier, NGBRegressor, NGBSurvival  # NOQA
 from .ngboost import NGBoost  # NOQA
+
+
+__version__ = version(__name__)
